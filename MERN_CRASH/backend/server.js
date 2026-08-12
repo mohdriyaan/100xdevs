7c6699e8e3af7be61c0a098e32854e0f20519817
mohdriyaan <mohammedrayaan1@gmail.com>
2026-08-12 00:10:24 +0000

    Learned Backend Authentication

---
+++ MERN_CRASH/backend/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+dotenv.config()
+
+connectDB()
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+// Parses raw json
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.use(cookieParser())
+
+app.use("/api/users",userRoutes)
+
+app.get("/",(req,res)=>{
+  res.send("Server is ready")
+})
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port,()=>{
+  console.log(`Server started on PORT ${port}`)
+})
+
