98b66221c185b588e863dcee537ad03e6ee7c702
mohdriyaan <mohammedrayaan1@gmail.com>
2026-08-14 03:43:04 +0000

    Added Frontend to MERN Auth

--- MERN_CRASH/backend/server.js
+++ MERN_CRASH/backend/server.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import cors from "cors"
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
+app.use(cors())
 app.use(cookieParser())
 
 app.use("/api/users",userRoutes)
---
+++ MERN_CRASH/frontend/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div>
+      <h1>MERN Auth</h1>
+    </div>
+  )
+}
+export default App
